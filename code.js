ee5e1dde6953b733e9adec81b324026d326833f8
Mateus Seehagen Rodrigues <mateusser@gmail.com>
2018-12-07 10:33:50 -0200

    Day 3 start

--- code.js
+++ code.js
@@ -1,30 +1,60 @@
 var fs = require('fs')
 
-var data = ''
+const FABRIC_SIZE = 1000
 
+var data = ''
 var readStream = fs.createReadStream('input.in', 'utf8')
 
 readStream.on('data', function(chunk) {  
   data += chunk
 }).on('end', function() {
-  const ids = data.split('\r\n')
-  let total2 = 0
-  let total3 = 0
-  ids.forEach(id => {
-    let sum2 = false
-    let sum3 = false
-    for (let i = 0; i < id.length; i = i + 1) {
-      const char = id.charAt(i)
-      const newStr = id.replace(RegExp(char, 'g'), '')
-      console.log(newStr)
-      const diff = id.length - newStr.length
-      if (diff === 2) sum2 = true
-      else if (diff === 3) sum3 = true
+  const pieces = data.split('\r\n')
+
+  let line = []
+  for (let i = 0; i < FABRIC_SIZE; i = i + 1) line.push(`.`)
+  let fabric = []
+  for (let i = 0; i < FABRIC_SIZE; i = i + 1) fabric.push(line)
+
+  pieces.forEach((piece, idx) => {
+    if (idx > 0) return
+    const [id, tail] = piece.split('@')
+    const [starts, sizes] = tail.split(':')
+    const [startX, startY] = starts.split(',')
+    const [width, height] = sizes.split('x')
+    console.log(`startX`, startX)
+    console.log(`startY`, startY)
+    console.log(`width`, width)
+    console.log(`height`, height)
+    let numAssing = 0
+    for (let y = 0; y < height; y = y + 1) {
+      for (let x = 0; x < width; x = x + 1) {
+        const yValue = y + parseInt(startY)
+        const xValue = x + parseInt(startX)
+        const curr = fabric[yValue][xValue]
+        // console.log(curr )
+        console.log(xValue, yValue)
+        if (curr.charCodeAt(0) === '.'.charCodeAt(0)) {
+          fabric[yValue].splice(xValue, 1, '#')
+        }
+        else if (curr.charCodeAt(0) === '#'.charCodeAt(0)) {
+          fabric[yValue].splice(xValue, 1, 'X')
+        }
+      numAssing = numAssing + 1
+      }
     }
-    if (sum2) total2 = total2 + 1
-    if (sum3) total3 = total3 + 1
+    console.log('num numAssing', numAssing)
   })
-  console.log(`Total 2: ${total2}`)
-  console.log(`Total 3: ${total3}`)
-  console.log(total2 * total3)
+  
+  let sum = 0
+  for (let y = 0; y < FABRIC_SIZE; y = y + 1) {
+    for (let x = 0; x < FABRIC_SIZE; x = x + 1) {
+      if (fabric[x][y] === 'X') sum = sum + 1
+    }
+  }
+
+  // const fb = fabric.join().replace(/,/g, '')
+  // console.log(fb)
+  // console.log(typeof fb)
+
+  console.log(sum)
 })
