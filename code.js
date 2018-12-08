4390e86b8d5256aef2232bb853a46aa9bfa799d5
mateusser <mateusser@gmail.com>
2018-12-08 18:13:54 -0200

    Decided to go with C, solved Day 3 challenge 1

--- code.js
+++ code.js
@@ -21,34 +21,44 @@ readStream.on('data', function(chunk) {
     const [starts, sizes] = tail.split(':')
     const [startX, startY] = starts.split(',')
     const [width, height] = sizes.split('x')
-    console.log(`startX`, startX)
-    console.log(`startY`, startY)
-    console.log(`width`, width)
-    console.log(`height`, height)
+    console.log(`startX  `, startX)
+    console.log(`startY   `, startY)
+    console.log(`width   `, width)
+    console.log(`height   `, height)
     let numAssing = 0
     for (let y = 0; y < height; y = y + 1) {
       for (let x = 0; x < width; x = x + 1) {
         const yValue = y + parseInt(startY)
         const xValue = x + parseInt(startX)
         const curr = fabric[yValue][xValue]
-        // console.log(curr )
-        console.log(xValue, yValue)
+        console.log(curr)
+        // console.log(xValue, yValue)
         if (curr.charCodeAt(0) === '.'.charCodeAt(0)) {
+          console.log(xValue, yValue)
+          console.log(`before`, fabric[yValue][xValue], fabric[yValue + 1][xValue], fabric[yValue + 2][xValue])
           fabric[yValue].splice(xValue, 1, '#')
+          console.log(`after`, fabric[yValue][xValue], fabric[yValue + 1][xValue], fabric[yValue + 2][xValue])
         }
         else if (curr.charCodeAt(0) === '#'.charCodeAt(0)) {
+          console.log(xValue, yValue)
+          console.log(`before`, fabric[yValue][xValue])
           fabric[yValue].splice(xValue, 1, 'X')
+          console.log(`after`, fabric[yValue][xValue])
         }
       numAssing = numAssing + 1
       }
     }
-    console.log('num numAssing', numAssing)
+    console.log('numAssing', numAssing)
   })
   
-  let sum = 0
+  let sumP = 0
+  let sumH = 0
+  let sumX = 0
   for (let y = 0; y < FABRIC_SIZE; y = y + 1) {
     for (let x = 0; x < FABRIC_SIZE; x = x + 1) {
-      if (fabric[x][y] === 'X') sum = sum + 1
+      if (fabric[y][x] === 'X') sumX = sumX + 1
+      else if (fabric[y][x] === '.') sumP = sumP + 1
+      else if (fabric[y][x] === '#') sumH = sumH + 1
     }
   }
 
@@ -56,5 +66,8 @@ readStream.on('data', function(chunk) {
   // console.log(fb)
   // console.log(typeof fb)
 
-  console.log(sum)
+  console.log('.', sumP)
+  console.log('#', sumH)
+  console.log('X', sumX)
+  console.log(sumP + sumH + sumX)
 })
